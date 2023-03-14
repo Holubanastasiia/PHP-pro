7f298c2f44643099286254fe69cce17310805238
Anastasia Holubnycha <anastastasia.holubnycha.cpamatica.io>
2023-03-14 12:02:38 +0200

    add control structure and include files

--- lesson-2/templates/footer.php
+++ lesson-2/templates/footer.php
@@ -5,7 +5,7 @@ $telNumber = '+380937078674';
 <footer>
     <div class="contact">
         <h1>Contact me: </h1>
-        <p>Email: <?= $email; ?></p>
-        <p>Tel: <?= $telNumber; ?></p>
+        <p>Email: <a href="mailto:<?= $email; ?>"><?= $email; ?></a></p>
+        <p>Tel: <a href="tel:<?= $telNumber; ?>"><?= $telNumber; ?></a></p>
     </div>
 </footer>
