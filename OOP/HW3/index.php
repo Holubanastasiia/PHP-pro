e24480ceaf4117f8afe39bb8948bb245c38d0fdb
Anastasia Holubnycha <anastastasia.holubnycha.cpamatica.io>
2023-05-20 14:17:32 +0300

    OOP autoload 3

--- OOP/HW3/index.php
+++ OOP/HW3/index.php
@@ -1,11 +1,12 @@
 <?php
 
 spl_autoload_register(function($className) {
+    $rootDir = 'src';
     $namespaceParts = explode('\\', $className);
     $rootNamespace = $namespaceParts[0];
 
     $filename = str_replace('\\', '/', $className);
-    $filename = str_replace($rootNamespace, 'src', $filename);
+    $filename = str_replace($rootNamespace, $rootDir, $filename);
 
     if (file_exists($filename .  '.php')) {
         require_once $filename . '.php';
